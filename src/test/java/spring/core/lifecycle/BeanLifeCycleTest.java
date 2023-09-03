591981ae7bb135e8a9ffb4d36a0c7770c705b65d
yurison <yu_ri_son@naver.com>
2023-09-03 21:28:30 +0900

    BeanLifeCycleTest - Null 반환

---
+++ src/test/java/spring/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package spring.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+class BeanLifeCycleTest {
+
+    @Test
+    void lifecycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://www.example.dev");
+            return networkClient;
+        }
+    }
+
+}
